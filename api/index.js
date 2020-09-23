5e1f4387862a817b8686cf669d5c0bc26f1efdfd
Simone Vittori <svitto@svit.to>
2020-09-23 16:37:51 +0100

    Delete name

--- api/index.js
+++ api/index.js
@@ -1,9 +1,5 @@
 module.exports = (req, res) => {
-    const {
-        query: { name },
-      } = req
-
     const {x, y} = req.body
 
-    res.send(`<h1>${name} Result</h1>${x} * ${y} = ${Number(x) * Number(y)}`)
+    res.send(`<h1>Result</h1>${x} * ${y} = ${Number(x) * Number(y)}`)
 }
